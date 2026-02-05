babd6d3e24fb53ef4ed247fbaa9f462eef147a8e
Nelson-Chinedu <egwuchinedu69@gmail.com>
2026-02-05 15:06:12 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "NovaOrg";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+    outputSystemJS: true,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+    ],
+    externals: ["single-spa", "react", "react-dom"],
+  });
+};
